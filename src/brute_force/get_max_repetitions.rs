8d472be5bb08996d8d6d81c28e9ec6e081536683
Anner <anner.wang@icloud.com>
2024-01-02 18:05:13 +0800

    add daily algorithm problem on leetcode

--- src/brute_force/get_max_repetitions.rs
+++ src/brute_force/get_max_repetitions.rs
@@ -3,9 +3,12 @@ use crate::brute_force::Solution;
 // https://leetcode.cn/problems/count-the-repetitions/
 impl Solution {
     pub fn get_max_repetitions(s1: String, n1: i32, s2: String, n2: i32) -> i32 {
-        let str1 = (0..n1).map(|_| s1).collect();
-        let str2 = (0..n2).map(|_| s2).collect();
+        // let str1: String = (0..n1).map(|_| s1.clone()).collect();
+        // let str2: String = (0..n2).map(|_| s2.clone()).collect();
 
         -1
     }
 }
+
+#[test]
+fn test_it() {}
